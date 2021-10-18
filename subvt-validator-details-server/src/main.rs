095b928d4333cf0d93341d9689a146dac9163668
kukabi <kutsalbilgin@gmail.com>
2021-10-18 18:12:34 +0300

    Add validator details server crate.

---
+++ subvt-validator-details-server/src/main.rs
@@ -0,0 +1,14 @@
+//! See `./lib.rs` for details.
+
+use lazy_static::lazy_static;
+use subvt_service_common::Service;
+use subvt_validator_details_server::ValidatorDetailsServer;
+
+lazy_static! {
+    static ref SERVICE: ValidatorDetailsServer = ValidatorDetailsServer::default();
+}
+
+#[tokio::main]
+async fn main() {
+    SERVICE.start().await;
+}
