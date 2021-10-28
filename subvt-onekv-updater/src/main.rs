862400985fbf73c541b14098fdf1fb918a31e729
kukabi <kutsalbilgin@gmail.com>
2021-10-28 15:41:09 +0300

    Fix fmt.

--- subvt-onekv-updater/src/main.rs
+++ subvt-onekv-updater/src/main.rs
@@ -1,8 +1,8 @@
 //! See `./lib.rs` for details.
 
 use lazy_static::lazy_static;
-use subvt_service_common::Service;
 use subvt_onekv_updater::OneKVUpdater;
+use subvt_service_common::Service;
 
 lazy_static! {
     static ref SERVICE: OneKVUpdater = OneKVUpdater::default();
