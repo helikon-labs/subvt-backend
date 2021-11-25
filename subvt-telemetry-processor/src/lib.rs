9df73f15dd992e8f96c13f92d30058edb528d248
kukabi <kutsalbilgin@gmail.com>
2021-11-25 23:41:55 +0300

    Telemetry processor more feed message decoding.

--- subvt-telemetry-processor/src/lib.rs
+++ subvt-telemetry-processor/src/lib.rs
@@ -1,3 +1,5 @@
+// https://github.com/paritytech/substrate-telemetry/blob/master/backend/test_utils/src/feed_message_de.rs
+
 use crate::types::FeedMessage;
 use anyhow::Context;
 use async_trait::async_trait;
@@ -28,13 +30,12 @@ impl Service for TelemetryProcessor {
             .await
             .context("Failed to connect")?;
         debug!("Telemetry server websocket handshake has been successfully completed.");
-        let msg = ws_stream
+        let _msg = ws_stream
             .next()
             .await
             .ok_or("didn't receive anything")
             .unwrap()
             .unwrap();
-        println!("#0 Received: {}", msg.into_text()?);
         ws_stream
             .send(Message::text(format!(
                 "subscribe:{}",
@@ -73,7 +74,6 @@ impl Service for TelemetryProcessor {
                 .ok_or("didn't receive anything")
                 .unwrap()
                 .unwrap();
-            println!("RECEIVED: {}", msg.clone().into_text()?);
             let v1 = &msg.into_data();
             let feed_messages = FeedMessage::from_bytes(v1)?;
             for feed_message in feed_messages {
@@ -107,7 +107,68 @@ impl Service for TelemetryProcessor {
                         location: _location,
                         startup_time: _startup_time,
                     } => {
-                        println!("Added node #: {} {:?}.", node_id, node);
+                        println!("Added node #{} :: {:?}.", node_id, node);
+                    }
+                    FeedMessage::RemovedNode { node_id } => {
+                        println!("Removed node #{}.", node_id);
+                    }
+                    FeedMessage::LocatedNode {
+                        node_id,
+                        latitude: _,
+                        longitude: _,
+                        city: _,
+                    } => {
+                        println!("Located node #{}.", node_id);
+                    }
+                    FeedMessage::NodeImportedBlock {
+                        node_id,
+                        block_details,
+                    } => {
+                        println!(
+                            "Node #{} imported block #{}.",
+                            node_id, block_details.block_number
+                        );
+                    }
+                    FeedMessage::NodeFinalizedBlock {
+                        node_id,
+                        block_number,
+                        block_hash: _,
+                    } => {
+                        println!("Node #{} finalized block #{}.", node_id, block_number);
+                    }
+                    FeedMessage::NodeStatsUpdate { node_id, stats } => {
+                        println!("Node #{} status {:?}.", node_id, stats);
+                    }
+                    FeedMessage::NodeHardware { node_id, hardware } => {
+                        println!("Node #{} hardware {:?}.", node_id, hardware);
+                    }
+                    FeedMessage::TimeSync { time } => {
+                        println!("Time sync :: {}", time);
+                    }
+                    FeedMessage::AddedChain {
+                        name,
+                        genesis_hash,
+                        node_count,
+                    } => {
+                        println!("Added chain {} {} {}", name, genesis_hash, node_count);
+                    }
+                    FeedMessage::RemovedChain { genesis_hash } => {
+                        println!("Removed chain {}", genesis_hash);
+                    }
+                    FeedMessage::SubscribedTo { genesis_hash } => {
+                        println!("Subscribed to chain {}", genesis_hash);
+                    }
+                    FeedMessage::UnsubscribedFrom { genesis_hash } => {
+                        println!("Unsubscribed from chain {}", genesis_hash);
+                    }
+                    FeedMessage::Pong { message } => {
+                        println!("Pong :: {}", message);
+                    }
+                    FeedMessage::StaleNode { node_id } => {
+                        println!("Stale node #{}.", node_id);
+                    }
+                    FeedMessage::NodeIOUpdate { node_id, io } => {
+                        println!("IO update #{} :: {:?}", node_id, io);
                     }
                     _ => (),
                 }
--- subvt-telemetry-processor/src/types.rs
+++ subvt-telemetry-processor/src/types.rs
@@ -39,7 +39,7 @@ pub struct NodeDetails {
     name: String,
     implementation: String,
     version: String,
-    stash_address: Option<String>,
+    controller_address: Option<String>,
     network_id: Option<String>,
 }
 
@@ -51,12 +51,11 @@ pub struct Block {
 
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct BlockDetails {
-    block_number: u64,
-    block_hash: String,
-    block_time: u64,
-    // block timestamp
-    block_timestamp: u64,
-    propagation_time: Option<u64>,
+    pub block_number: u64,
+    pub block_hash: String,
+    pub block_time: u64,
+    pub block_timestamp: u64,
+    pub propagation_time: Option<u64>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -80,6 +79,84 @@ pub enum FeedMessage {
         block_details: BlockDetails,
         location: Option<NodeLocation>,
         startup_time: Option<u64>,
+    },
+    RemovedNode {
+        node_id: usize,
+    },
+    LocatedNode {
+        node_id: usize,
+        latitude: f32,
+        longitude: f32,
+        city: String,
+    },
+    NodeImportedBlock {
+        node_id: usize,
+        block_details: BlockDetails,
+    },
+    NodeFinalizedBlock {
+        node_id: usize,
+        block_number: u64,
+        block_hash: String,
+    },
+    NodeStatsUpdate {
+        node_id: usize,
+        stats: NodeStats,
+    },
+    NodeHardware {
+        node_id: usize,
+        hardware: NodeHardware,
+    },
+    TimeSync {
+        time: u64,
+    },
+    AddedChain {
+        name: String,
+        genesis_hash: String,
+        node_count: usize,
+    },
+    RemovedChain {
+        genesis_hash: String,
+    },
+    SubscribedTo {
+        genesis_hash: String,
+    },
+    UnsubscribedFrom {
+        genesis_hash: String,
+    },
+    Pong {
+        message: String,
+    },
+    AfgFinalized {
+        address: String,
+        block_number: u64,
+        block_hash: String,
+    },
+    AfgReceivedPrevote {
+        address: String,
+        block_number: u64,
+        block_hash: String,
+        voter: Option<String>,
+    },
+    AfgReceivedPrecommit {
+        address: String,
+        block_number: u64,
+        block_hash: String,
+        voter: Option<String>,
+    },
+    AfgAuthoritySet {
+        // Not used currently; not sure what "address" params are:
+        a1: String,
+        a2: String,
+        a3: String,
+        block_number: u64,
+        block_hash: String,
+    },
+    StaleNode {
+        node_id: usize,
+    },
+    NodeIOUpdate {
+        node_id: usize,
+        io: NodeIO,
     },
     UnknownValue {
         action: u8,
@@ -132,7 +209,7 @@ impl FeedMessage {
             3 => {
                 let (
                     node_id,
-                    (name, implementation, version, stash_address, network_id),
+                    (name, implementation, version, controller_address, network_id),
                     (peer_count, queued_tx_count),
                     (state_cache_sizes,),
                     (upload_bandwidths, download_bandwidths, timestamps),
@@ -146,7 +223,7 @@ impl FeedMessage {
                         name,
                         implementation,
                         version,
-                        stash_address,
+                        controller_address,
                         network_id,
                     }),
                     stats: NodeStats {
@@ -168,6 +245,141 @@ impl FeedMessage {
                     },
                     location,
                     startup_time,
+                }
+            }
+            // removed node
+            4 => {
+                let node_id = serde_json::from_str(raw_value.get())?;
+                FeedMessage::RemovedNode { node_id }
+            }
+            // located node
+            5 => {
+                let (node_id, latitude, longitude, city) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::LocatedNode {
+                    node_id,
+                    latitude,
+                    longitude,
+                    city,
+                }
+            }
+            // node imported block
+            6 => {
+                let (node_id, block_details) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::NodeImportedBlock {
+                    node_id,
+                    block_details,
+                }
+            }
+            // node finalized block
+            7 => {
+                let (node_id, block_number, block_hash) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::NodeFinalizedBlock {
+                    node_id,
+                    block_number,
+                    block_hash,
+                }
+            }
+            // node stats update
+            8 => {
+                let (node_id, stats) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::NodeStatsUpdate { node_id, stats }
+            }
+            // node hardware
+            9 => {
+                let (node_id, (upload_bandwidths, download_bandwidths, timestamps)) =
+                    serde_json::from_str(raw_value.get())?;
+                FeedMessage::NodeHardware {
+                    node_id,
+                    hardware: NodeHardware(upload_bandwidths, download_bandwidths, timestamps),
+                }
+            }
+            // time sync
+            10 => {
+                let time = serde_json::from_str(raw_value.get())?;
+                FeedMessage::TimeSync { time }
+            }
+            // added chain
+            11 => {
+                let (name, genesis_hash, node_count) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::AddedChain {
+                    name,
+                    genesis_hash,
+                    node_count,
+                }
+            }
+            // removed chain
+            12 => {
+                let genesis_hash = serde_json::from_str(raw_value.get())?;
+                FeedMessage::RemovedChain { genesis_hash }
+            }
+            // subscribed to
+            13 => {
+                let genesis_hash = serde_json::from_str(raw_value.get())?;
+                FeedMessage::SubscribedTo { genesis_hash }
+            }
+            // unsubscribed from
+            14 => {
+                let genesis_hash = serde_json::from_str(raw_value.get())?;
+                FeedMessage::UnsubscribedFrom { genesis_hash }
+            }
+            // Pong
+            15 => {
+                let message = serde_json::from_str(raw_value.get())?;
+                FeedMessage::Pong { message }
+            }
+            // afg finalized
+            16 => {
+                let (address, block_number, block_hash) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::AfgFinalized {
+                    address,
+                    block_number,
+                    block_hash,
+                }
+            }
+            // afg received prevote
+            17 => {
+                let (address, block_number, block_hash, voter) =
+                    serde_json::from_str(raw_value.get())?;
+                FeedMessage::AfgReceivedPrevote {
+                    address,
+                    block_number,
+                    block_hash,
+                    voter,
+                }
+            }
+            // afg received precommit
+            18 => {
+                let (address, block_number, block_hash, voter) =
+                    serde_json::from_str(raw_value.get())?;
+                FeedMessage::AfgReceivedPrecommit {
+                    address,
+                    block_number,
+                    block_hash,
+                    voter,
+                }
+            }
+            // afg authority set
+            19 => {
+                let (a1, a2, a3, block_number, block_hash) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::AfgAuthoritySet {
+                    a1,
+                    a2,
+                    a3,
+                    block_number,
+                    block_hash,
+                }
+            }
+            // stale node
+            20 => {
+                let node_id = serde_json::from_str(raw_value.get())?;
+                FeedMessage::StaleNode { node_id }
+            }
+            // node io
+            21 => {
+                let (node_id, (state_cache_sizes,)) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::NodeIOUpdate {
+                    node_id,
+                    io: NodeIO(state_cache_sizes),
                 }
             }
             // A catchall for messages we don't know/care about yet:
