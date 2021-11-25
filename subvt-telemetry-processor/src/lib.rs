833ed9a52152c5cd2704ba507ff780d7d3b75169
kukabi <kutsalbilgin@gmail.com>
2021-11-25 19:55:01 +0300

    More work around Telemetry feed message decoding.

--- subvt-telemetry-processor/src/lib.rs
+++ subvt-telemetry-processor/src/lib.rs
@@ -1,10 +1,10 @@
+use crate::types::FeedMessage;
 use anyhow::Context;
 use async_trait::async_trait;
 use async_tungstenite::{tokio::connect_async, tungstenite::Message};
 use futures::{SinkExt, StreamExt};
 use lazy_static::lazy_static;
 use log::{debug, info};
-use serde_json::value::RawValue;
 use subvt_config::Config;
 use subvt_service_common::Service;
 
@@ -19,40 +19,11 @@ pub struct TelemetryProcessor;
 
 impl TelemetryProcessor {}
 
-/*
-struct FeedMessage(u8);
-struct AddNodeMessage(u8);
-
-impl<'de> serde::Deserialize<'de> for FeedMessage {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let value = Value::deserialize(deserializer)?;
-        match value.get(0).and_then(Value::as_u64).unwrap() {
-            3 => println!("ADD NODE"),
-            unsupported_type => println!("unsupported type {:?}", unsupported_type),
-        }
-        // println!("value :: {:?}", value);
-        Ok(FeedMessage(3))
-    }
-}
-
- */
-
-pub enum FeedMessage {
-    BestBlock {
-        block_number: u64,
-        timestamp: u64,
-        avg_block_time: Option<u64>,
-    },
-}
-
 #[async_trait(?Send)]
 impl Service for TelemetryProcessor {
     async fn run(&'static self) -> anyhow::Result<()> {
         info!("Running the Telemetry processor.");
-
+        // connect to Telemetry feed
         let (mut ws_stream, _) = connect_async("wss://telemetry.w3f.community/feed")
             .await
             .context("Failed to connect")?;
@@ -87,6 +58,13 @@ impl Service for TelemetryProcessor {
             .unwrap()
             .unwrap();
         println!("#1 Received: {}", msg.into_text()?);
+        let msg = ws_stream
+            .next()
+            .await
+            .ok_or("didn't receive anything")
+            .unwrap()
+            .unwrap();
+        println!("#2 Received: {}", msg.into_text()?);
 
         loop {
             let msg = ws_stream
@@ -95,22 +73,43 @@ impl Service for TelemetryProcessor {
                 .ok_or("didn't receive anything")
                 .unwrap()
                 .unwrap();
+            println!("RECEIVED: {}", msg.clone().into_text()?);
             let v1 = &msg.into_data();
-            let v: Vec<&RawValue> = serde_json::from_slice(v1)?;
-            // https://github.com/paritytech/substrate-telemetry/blob/a4069e4b3d6f74d1cacdd3f2af50cb7105943cd0/backend/test_utils/src/feed_message_de.rs
-            for raw_keyval in v.chunks(2) {
-                let raw_key = raw_keyval[0];
-                let raw_val = raw_keyval[1];
-                let action: u8 = serde_json::from_str(raw_key.get())?;
-                if action == 1 {
-                    let (block_number, timestamp, avg_block_time) =
-                        serde_json::from_str(raw_val.get())?;
-                    let _message = FeedMessage::BestBlock {
+            let feed_messages = FeedMessage::from_bytes(v1)?;
+            for feed_message in feed_messages {
+                match feed_message {
+                    FeedMessage::Version(version) => {
+                        println!("Version: {}.", version);
+                    }
+                    FeedMessage::BestBlock {
                         block_number,
                         timestamp,
                         avg_block_time,
-                    };
-                    println!("BEST BLOCK {}", block_number);
+                    } => {
+                        println!(
+                            "Best block: {} {} {:?}.",
+                            block_number, timestamp, avg_block_time
+                        );
+                    }
+                    FeedMessage::BestFinalized {
+                        block_number,
+                        block_hash,
+                    } => {
+                        println!("Best finalized: {} {}.", block_number, block_hash);
+                    }
+                    FeedMessage::AddedNode {
+                        node_id,
+                        node,
+                        stats: _stats,
+                        io: _io,
+                        hardware: _hardware,
+                        block_details: _block_details,
+                        location: _location,
+                        startup_time: _startup_time,
+                    } => {
+                        println!("Added node #: {} {:?}.", node_id, node);
+                    }
+                    _ => (),
                 }
             }
         }
--- subvt-telemetry-processor/src/types.rs
+++ subvt-telemetry-processor/src/types.rs
@@ -1,94 +1,181 @@
+use anyhow::Context;
 use serde::Deserialize;
+use serde_json::value::RawValue;
 
-pub type BlockNumber = u64;
-pub type Timestamp = u64;
-pub type BlockHash = String;
-pub type FeedNodeId = usize;
-pub type MeanList<T> = [T; 20];
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct NodeStats {
+    peer_count: u64,
+    queued_tx_count: u64,
+}
 
-#[derive(Deserialize)]
-pub struct NodeDetails(
-    // name
-    Box<str>,
-    // implementation
-    Box<str>,
-    // version
-    Box<str>,
-    // validator
-    Option<Box<str>>,
-    //network_id
-    Option<Box<str>>,
-    // startup_time
-    Option<Box<str>>,
-);
-
-#[derive(Deserialize)]
-pub struct NodeStats(
-    // peers
-    u64,
-    // txcount
-    u64,
-);
-
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct NodeIO(
-    // used_state_cache_size
-    MeanList<f32>,
+    // state cache size
+    Vec<f32>,
 );
 
-#[derive(Deserialize)]
-pub struct Block(
-    // hash
-    BlockHash,
-    // height
-    BlockNumber,
-);
-
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct NodeHardware(
-    // upload
-    MeanList<f64>,
-    // download
-    MeanList<f64>,
-    // chart_stamps
-    MeanList<f64>,
+    // upload bandwidth
+    Vec<f64>,
+    // download bandwidth
+    Vec<f64>,
+    // timestamps
+    Vec<f64>,
 );
 
-#[derive(Deserialize)]
-pub struct BlockDetails(
-    BlockNumber,
-    BlockHash,
-    // block_time
-    u64,
-    // block_timestamp
-    u64,
-    // propagation_time
-    Option<u64>,
-);
-
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct NodeLocation(
     // latitude
     f32,
     // longitude
     f32,
     // city
-    Box<str>,
+    String,
 );
 
-#[derive(Deserialize)]
-pub struct Node(
-    NodeDetails,
-    NodeStats,
-    NodeIO,
-    NodeHardware,
-    BlockDetails, // best
-    Block,        // finalized
-    u64,          // throttle
-    Option<NodeLocation>,
-    bool,              // stale
-    Option<Timestamp>, // startup time
-);
+#[derive(Debug, PartialEq)]
+pub struct NodeDetails {
+    name: String,
+    implementation: String,
+    version: String,
+    stash_address: Option<String>,
+    network_id: Option<String>,
+}
 
 #[derive(Deserialize)]
-pub struct AddedNode(pub FeedNodeId, pub Node);
+pub struct Block {
+    _block_hash: String,
+    _block_height: u64,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+pub struct BlockDetails {
+    block_number: u64,
+    block_hash: String,
+    block_time: u64,
+    // block timestamp
+    block_timestamp: u64,
+    propagation_time: Option<u64>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum FeedMessage {
+    Version(usize),
+    BestBlock {
+        block_number: u64,
+        timestamp: u64,
+        avg_block_time: Option<u64>,
+    },
+    BestFinalized {
+        block_number: u64,
+        block_hash: String,
+    },
+    AddedNode {
+        node_id: usize,
+        node: Box<NodeDetails>,
+        stats: NodeStats,
+        io: NodeIO,                  // can't losslessly deserialize
+        hardware: Box<NodeHardware>, // can't losslessly deserialize
+        block_details: BlockDetails,
+        location: Option<NodeLocation>,
+        startup_time: Option<u64>,
+    },
+    UnknownValue {
+        action: u8,
+        value: String,
+    },
+}
+
+impl FeedMessage {
+    pub fn from_bytes(bytes: &[u8]) -> anyhow::Result<Vec<FeedMessage>> {
+        let v: Vec<&RawValue> = serde_json::from_slice(bytes)?;
+        let mut feed_messages = vec![];
+        for raw_key_value in v.chunks(2) {
+            let raw_key = raw_key_value[0];
+            let raw_value = raw_key_value[1];
+            let action: u8 = serde_json::from_str(raw_key.get())?;
+            let msg = FeedMessage::decode(action, raw_value).with_context(|| {
+                format!("Failed to decode feed message with action {}.", action)
+            })?;
+            feed_messages.push(msg);
+        }
+        Ok(feed_messages)
+    }
+
+    fn decode(action: u8, raw_value: &RawValue) -> anyhow::Result<FeedMessage> {
+        let feed_message = match action {
+            // version
+            0 => {
+                let version = serde_json::from_str(raw_value.get())?;
+                FeedMessage::Version(version)
+            }
+            // best block
+            1 => {
+                let (block_number, timestamp, avg_block_time) =
+                    serde_json::from_str(raw_value.get())?;
+                FeedMessage::BestBlock {
+                    block_number,
+                    timestamp,
+                    avg_block_time,
+                }
+            }
+            // best finalized
+            2 => {
+                let (block_number, block_hash) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::BestFinalized {
+                    block_number,
+                    block_hash,
+                }
+            }
+            // added node
+            3 => {
+                let (
+                    node_id,
+                    (name, implementation, version, stash_address, network_id),
+                    (peer_count, queued_tx_count),
+                    (state_cache_sizes,),
+                    (upload_bandwidths, download_bandwidths, timestamps),
+                    (block_number, block_hash, block_time, block_timestamp, propagation_time),
+                    location,
+                    startup_time,
+                ) = serde_json::from_str(raw_value.get())?;
+                FeedMessage::AddedNode {
+                    node_id,
+                    node: Box::new(NodeDetails {
+                        name,
+                        implementation,
+                        version,
+                        stash_address,
+                        network_id,
+                    }),
+                    stats: NodeStats {
+                        peer_count,
+                        queued_tx_count,
+                    },
+                    io: NodeIO(state_cache_sizes),
+                    hardware: Box::new(NodeHardware(
+                        upload_bandwidths,
+                        download_bandwidths,
+                        timestamps,
+                    )),
+                    block_details: BlockDetails {
+                        block_number,
+                        block_hash,
+                        block_time,
+                        block_timestamp,
+                        propagation_time,
+                    },
+                    location,
+                    startup_time,
+                }
+            }
+            // A catchall for messages we don't know/care about yet:
+            _ => {
+                let value = raw_value.to_string();
+                FeedMessage::UnknownValue { action, value }
+            }
+        };
+        Ok(feed_message)
+    }
+}
