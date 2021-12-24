848645b595f8da232c035919e0cf010f8db6dd99
kukabi <kutsalbilgin@gmail.com>
2021-12-24 15:07:50 +0300

    Fix clippy.

--- subvt-substrate-client/tests/test.rs
+++ subvt-substrate-client/tests/test.rs
@@ -11,11 +11,9 @@ async fn test_get_block_hash() {
     let hash = substrate_client
         .get_block_hash(block_number)
         .await
-        .expect(&format!(
-            "Cannot get block hash for block #{}.",
-            block_number
-        ));
-    let hash = hex::decode(hash.trim_start_matches("0x")).expect("Cannot decode hash from client.");
+        .unwrap_or_else(|_| panic!("Cannot get block hash for block #{}.", block_number));
+    let hash = hex::decode(hash.trim_start_matches("0x"))
+        .unwrap_or_else(|_| panic!("Cannot decode hash from client."));
     let expected_hash =
         hex::decode("9d95763d4119488779991da8d1b16874687a3308ffcf9f89284d0382e8ccd161").unwrap();
     assert_eq!(hash, expected_hash);
