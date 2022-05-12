de57bbb6e89663d9f03db5a6a6c11145c3b977ad
kukabi <kutsalbilgin@gmail.com>
2022-05-12 14:18:38 +0300

    Fix minor bugs in notification template (grouped lost & new nominations) and Docker compose files.

--- _docker/compose/down.sh
+++ _docker/compose/down.sh
@@ -3,5 +3,5 @@ docker-compose -p subvt_polkadot_services -f 06-docker-compose-subvt-polkadot-se
 docker-compose -p subvt_kusama_services -f 05-docker-compose-subvt-kusama-services.yml down
 docker-compose -p subvt_app_services -f 04-docker-compose-subvt-app-services.yml down
 docker-compose -p subvt_app_data_services -f 03-docker-compose-app-data.yml down
-docker-compose -p subvt_kusama_data_services -f 02-docker-compose-polkadot-data.yml down
+docker-compose -p subvt_polkadot_data_services -f 02-docker-compose-polkadot-data.yml down
 docker-compose -p subvt_kusama_data_services -f 01-docker-compose-kusama-data.yml down
