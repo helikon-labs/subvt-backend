49ab91cb857403126d9b90fe40de9f348e207e56
kukabi <kutsalbilgin@gmail.com>
2022-05-12 11:12:27 +0300

    Add up & down scripts for all Docker compose files.

---
+++ _docker/compose/up.sh
@@ -0,0 +1,7 @@
+docker-compose -p subvt_kusama_data_services -f 01-docker-compose-kusama-data.yml up -d
+docker-compose -p subvt_polkadot_data_services -f 02-docker-compose-polkadot-data.yml up -d
+docker-compose -p subvt_app_data_services -f 03-docker-compose-app-data.yml up -d
+docker-compose -p subvt_app_services -f 04-docker-compose-subvt-app-services.yml up -d
+docker-compose -p subvt_kusama_services -f 05-docker-compose-subvt-kusama-services.yml up -d
+docker-compose -p subvt_polkadot_services -f 06-docker-compose-subvt-polkadot-services.yml up -d
+docker-compose -p subvt_monitoring_services -f 07-docker-compose-subvt-monitoring.yml up -d
