c172d4f0dbbb364c026c3b0f49685e7347b46538
kukabi <kutsalbilgin@gmail.com>
2022-06-15 10:36:47 +0300

    Update Docker publish script to include the latest tag with all images.

--- _docker/docker-build-and-publish.sh
+++ _docker/docker-build-and-publish.sh
@@ -15,56 +15,56 @@ fi
 cd "${0%/*}" || exit
 
 # backend base
-docker build -t helikon/subvt-backend-base:"$1" --no-cache --build-arg version="$1" -f ./base/02-subvt-backend-base.dockerfile ..
+docker build -t helikon/subvt-backend-base:"$1" -t helikon/subvt-backend-base:latest --no-cache --build-arg version="$1" -f ./base/02-subvt-backend-base.dockerfile ..
 # backend lib
-docker build -t helikon/subvt-backend-lib:"$1" --no-cache --build-arg version="$1" -f ./base/01-subvt-backend-lib.dockerfile ..
+docker build -t helikon/subvt-backend-lib:"$1" -t helikon/subvt-backend-lib:latest --no-cache --build-arg version="$1" -f ./base/01-subvt-backend-lib.dockerfile ..
 
 # app postgres
-docker build -t helikon/subvt-app-postgres:"$1" --no-cache --build-arg version="$1" -f ./app/01-subvt-app-postgres.dockerfile ..
-docker push helikon/subvt-app-postgres:"$1"
+docker build -t helikon/subvt-app-postgres:"$1" -t helikon/subvt-app-postgres:latest --no-cache --build-arg version="$1" -f ./app/01-subvt-app-postgres.dockerfile ..
+docker push --all-tags helikon/subvt-app-postgres
 # app service
-docker build -t helikon/subvt-app-service:"$1" --no-cache --build-arg version="$1" -f ./app/02-subvt-app-service.dockerfile ..
-docker push helikon/subvt-app-service:"$1"
+docker build -t helikon/subvt-app-service:"$1" -t helikon/subvt-app-service:latest --no-cache --build-arg version="$1" -f ./app/02-subvt-app-service.dockerfile ..
+docker push --all-tags helikon/subvt-app-service
 # notification processor
-docker build -t helikon/subvt-notification-processor:"$1" --no-cache --build-arg version="$1" -f ./app/03-subvt-notification-processor.dockerfile ..
-docker push helikon/subvt-notification-processor:"$1"
+docker build -t helikon/subvt-notification-processor:"$1" -t helikon/subvt-notification-processor:latest --no-cache --build-arg version="$1" -f ./app/03-subvt-notification-processor.dockerfile ..
+docker push --all-tags helikon/subvt-notification-processor
 
 # network postgres
-docker build -t helikon/subvt-network-postgres:"$1" --no-cache --build-arg version="$1" -f ./network/01-subvt-network-postgres.dockerfile ..
-docker push helikon/subvt-network-postgres:"$1"
+docker build -t helikon/subvt-network-postgres:"$1" -t helikon/subvt-network-postgres:latest --no-cache --build-arg version="$1" -f ./network/01-subvt-network-postgres.dockerfile ..
+docker push --all-tags helikon/subvt-network-postgres
 # block processor
-docker build -t helikon/subvt-block-processor:"$1" --no-cache --build-arg version="$1" -f ./network/02-subvt-block-processor.dockerfile ..
-docker push helikon/subvt-block-processor:"$1"
+docker build -t helikon/subvt-block-processor:"$1" -t helikon/subvt-block-processor:latest --no-cache --build-arg version="$1" -f ./network/02-subvt-block-processor.dockerfile ..
+docker push --all-tags helikon/subvt-block-processor
 # validator list updater
-docker build -t helikon/subvt-validator-list-updater:"$1" --no-cache --build-arg version="$1" -f ./network/03-subvt-validator-list-updater.dockerfile ..
-docker push helikon/subvt-validator-list-updater:"$1"
+docker build -t helikon/subvt-validator-list-updater:"$1" -t helikon/subvt-validator-list-updater:latest --no-cache --build-arg version="$1" -f ./network/03-subvt-validator-list-updater.dockerfile ..
+docker push --all-tags helikon/subvt-validator-list-updater
 # active validator list server
-docker build -t helikon/subvt-active-validator-list-server:"$1" --no-cache --build-arg version="$1" -f ./network/04-subvt-active-validator-list-server.dockerfile ..
-docker push helikon/subvt-active-validator-list-server:"$1"
+docker build -t helikon/subvt-active-validator-list-server:"$1" -t helikon/subvt-active-validator-list-server:latest --no-cache --build-arg version="$1" -f ./network/04-subvt-active-validator-list-server.dockerfile ..
+docker push --all-tags helikon/subvt-active-validator-list-server
 # inactive validator list server
-docker build -t helikon/subvt-inactive-validator-list-server:"$1" --no-cache --build-arg version="$1" -f ./network/05-subvt-inactive-validator-list-server.dockerfile ..
-docker push helikon/subvt-inactive-validator-list-server:"$1"
+docker build -t helikon/subvt-inactive-validator-list-server:"$1" -t helikon/subvt-inactive-validator-list-server:latest --no-cache --build-arg version="$1" -f ./network/05-subvt-inactive-validator-list-server.dockerfile ..
+docker push --all-tags helikon/subvt-inactive-validator-list-server
 # validator details server
-docker build -t helikon/subvt-validator-details-server:"$1" --no-cache --build-arg version="$1" -f ./network/06-subvt-validator-details-server.dockerfile ..
-docker push helikon/subvt-validator-details-server:"$1"
+docker build -t helikon/subvt-validator-details-server:"$1" -t helikon/subvt-validator-details-server:latest --no-cache --build-arg version="$1" -f ./network/06-subvt-validator-details-server.dockerfile ..
+docker push --all-tags helikon/subvt-validator-details-server
 # network status updater
-docker build -t helikon/subvt-network-status-updater:"$1" --no-cache --build-arg version="$1" -f ./network/07-subvt-network-status-updater.dockerfile ..
-docker push helikon/subvt-network-status-updater:"$1"
+docker build -t helikon/subvt-network-status-updater:"$1" -t helikon/subvt-network-status-updater:latest --no-cache --build-arg version="$1" -f ./network/07-subvt-network-status-updater.dockerfile ..
+docker push --all-tags helikon/subvt-network-status-updater
 # network status server
-docker build -t helikon/subvt-network-status-server:"$1" --no-cache --build-arg version="$1" -f ./network/08-subvt-network-status-server.dockerfile ..
-docker push helikon/subvt-network-status-server:"$1"
+docker build -t helikon/subvt-network-status-server:"$1" -t helikon/subvt-network-status-server:latest --no-cache --build-arg version="$1" -f ./network/08-subvt-network-status-server.dockerfile ..
+docker push --all-tags helikon/subvt-network-status-server
 # onekv updater
-docker build -t helikon/subvt-onekv-updater:"$1" --no-cache --build-arg version="$1" -f ./network/09-subvt-onekv-updater.dockerfile ..
-docker push helikon/subvt-onekv-updater:"$1"
+docker build -t helikon/subvt-onekv-updater:"$1" -t helikon/subvt-onekv-updater:latest --no-cache --build-arg version="$1" -f ./network/09-subvt-onekv-updater.dockerfile ..
+docker push --all-tags helikon/subvt-onekv-updater
 # telemetry processor
-docker build -t helikon/subvt-telemetry-processor:"$1" --no-cache --build-arg version="$1" -f ./network/10-subvt-telemetry-processor.dockerfile ..
-docker push helikon/subvt-telemetry-processor:"$1"
+docker build -t helikon/subvt-telemetry-processor:"$1" -t helikon/subvt-telemetry-processor:latest --no-cache --build-arg version="$1" -f ./network/10-subvt-telemetry-processor.dockerfile ..
+docker push --all-tags helikon/subvt-telemetry-processor
 # telegram bot
-docker build -t helikon/subvt-telegram-bot:"$1" --no-cache --build-arg version="$1" -f ./network/11-subvt-telegram-bot.dockerfile ..
-docker push helikon/subvt-telegram-bot:"$1"
+docker build -t helikon/subvt-telegram-bot:"$1" -t helikon/subvt-telegram-bot:latest --no-cache --build-arg version="$1" -f ./network/11-subvt-telegram-bot.dockerfile ..
+docker push --all-tags helikon/subvt-telegram-bot
 # notification generator
-docker build -t helikon/subvt-notification-generator:"$1" --no-cache --build-arg version="$1" -f ./network/12-subvt-notification-generator.dockerfile ..
-docker push helikon/subvt-notification-generator:"$1"
+docker build -t helikon/subvt-notification-generator:"$1" -t helikon/subvt-notification-generator:latest --no-cache --build-arg version="$1" -f ./network/12-subvt-notification-generator.dockerfile ..
+docker push --all-tags helikon/subvt-notification-generator
 # report service
-docker build -t helikon/subvt-report-service:"$1" --no-cache --build-arg version="$1" -f ./network/13-subvt-report-service.dockerfile ..
-docker push helikon/subvt-report-service:"$1"
+docker build -t helikon/subvt-report-service:"$1" -t helikon/subvt-report-service:latest --no-cache --build-arg version="$1" -f ./network/13-subvt-report-service.dockerfile ..
+docker push --all-tags helikon/subvt-report-service
