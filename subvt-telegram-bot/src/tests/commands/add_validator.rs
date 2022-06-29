d4536118f5989e4d0912f992baa1a6d56ae95f81
kukabi <kutsalbilgin@gmail.com>
2022-06-29 11:13:03 +0300

    Fix fmt.

--- subvt-telegram-bot/src/tests/commands/add_validator.rs
+++ subvt-telegram-bot/src/tests/commands/add_validator.rs
@@ -1,8 +1,8 @@
-use frankenstein::MethodResponse;
-use crate::MessageType;
 use crate::messenger::MockMessenger;
-use crate::tests::util::{get_random_chat_id, new_test_bot};
 use crate::tests::util::data::get_telegram_response_message;
+use crate::tests::util::{get_random_chat_id, new_test_bot};
+use crate::MessageType;
+use frankenstein::MethodResponse;
 
 #[tokio::test]
 #[allow(clippy::borrowed_box)]
@@ -16,20 +16,17 @@ async fn test_add_non_existent_validator() {
     };
     messenger
         .expect_send_message()
-        .withf(move |_, _, _, message_type: &Box<MessageType>| {
-            match &**message_type {
+        .withf(
+            move |_, _, _, message_type: &Box<MessageType>| match &**message_type {
                 MessageType::InvalidAddress(account_id) => account_id == "0xABC",
-                _ => false
-            }
-        })
+                _ => false,
+            },
+        )
         .return_once(move |_, _, _, _| Ok(response));
     let bot = new_test_bot(messenger).await.unwrap();
     assert!(bot.save_or_restore_chat(chat_id).await.is_ok());
-    assert!(
-        bot.process_command(
-            chat_id,
-            "/add",
-            &["0xABC".to_string()]
-        ).await.is_ok()
-    );
+    assert!(bot
+        .process_command(chat_id, "/add", &["0xABC".to_string()])
+        .await
+        .is_ok());
 }
